53be20042ed789a94aa0ecabe32cc7bbd8e60643
Kylan Baksh <123013138+KaiesoKB@users.noreply.github.com>
2025-10-27 12:39:32 -0400

    Update Loan_Acceptance_Predictor_App.py

--- Loan_Acceptance_Project/app/Loan_Acceptance_Predictor_App.py
+++ Loan_Acceptance_Project/app/Loan_Acceptance_Predictor_App.py
@@ -3,10 +3,10 @@ import numpy as np
 import streamlit as st
 import pickle
 
-with open("Model_scaler.pkl", "rb") as f:
+with open("Loan_Acceptance_Project/app/Model_scaler.pkl", "rb") as f:
     scaler = pickle.load(f)
 
-with open("Personal_loan_acceptance_Prediction_Model.pkl", "rb") as f:
+with open("Loan_Acceptance_Project/app/Model_random_forest.pkl", "rb") as f:
     model = pickle.load(f)
 
 st.title("Personal Loan Acceptance Predictor")
@@ -49,3 +49,4 @@ if st.button("Predict Loan Acceptance"):
     
     st.success(f"Prediction of Accepting Personal Loan Offer: {'☑️Yes' if pred_class == 1 else '✖️No'}")
     st.info(f"Probability of Accepting Personal Loan Offer: {pred_prob:.4f}")
+
